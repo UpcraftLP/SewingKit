e7ab8d1469f1e50745e378d5b94257b7c516dde6
UpcraftLP <j-schirra@gmx.de>
2019-05-14 23:29:08 +0200

    misc improvements
    (stuff suggested by idea's static analysis)

--- src/main/java/com/blamejared/sewingkit/CommandHand.java
+++ src/main/java/com/blamejared/sewingkit/CommandHand.java
@@ -16,15 +16,13 @@ import java.io.File;
 public class CommandHand {
     
     public static void register(CommandDispatcher<ServerCommandSource> cd) {
-        String filepath = new File("sewingkit.log").getAbsolutePath();
-        Style style = new Style();
-        ClickEvent click = new ClickEvent(ClickEvent.Action.OPEN_FILE, filepath);
-        style.setClickEvent(click);
-        
-        HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslatableTextComponent("Click to open [\u00A76" + filepath + "\u00A7r]"));
-        style.setHoverEvent(hoverEvent);
-        
         cd.register(CommandManager.literal("skhand").requires((var0x) -> var0x.hasPermissionLevel(3)).executes((var0x) -> {
+            String filepath = new File("sewingkit.log").getAbsolutePath();
+            Style style = new Style();
+            ClickEvent click = new ClickEvent(ClickEvent.Action.OPEN_FILE, filepath);
+            style.setClickEvent(click);
+            HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslatableTextComponent("Click to open [\u00A76" + filepath + "\u00A7r]"));
+            style.setHoverEvent(hoverEvent);
             SKApi.logger.logCommand(new MCItemStack(var0x.getSource().getPlayer().inventory.getMainHandStack()).toCommandString());
             var0x.getSource().sendFeedback(new TranslatableTextComponent("Item: \u00A72" + new MCItemStack(var0x.getSource().getPlayer().inventory.getMainHandStack()).toCommandString() + "\u00A7a"), true);
             var0x.getSource().sendFeedback(new TranslatableTextComponent("Open log").setStyle(style), true);
